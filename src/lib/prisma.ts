2527438b78cf0b97689c00f1dfa74e96f16637bf
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-07-04 23:53:59 -0300

    feat: create prisma client for verify dev mod

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import {PrismaClient} from '@prisma/client'
+// Evitar que a cada re render ele estoure o banco.
+const globalForPrisma = global as unknown as {prisma: PrismaClient}
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:["query"],
+  })
+
+if(process.env.NODE_ENV !== "production") globalForPrisma.prisma
