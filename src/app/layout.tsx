63c6cd2d792d641f5595e5390435941119966342
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-07-05 23:55:52 -0300

    feat: add foot in layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { NextAuthProvider } from '@/providers/auth'
 import './globals.css'
 import { Poppins } from 'next/font/google'
-import { Header } from './components/Header'
+import { Header } from '@/components/Header'
+import Footer from '@/components/Footer'
+
 
 const poppins = Poppins({ subsets: ['latin'], weight:[
   "400" , "500" , "600" , "700" , "800" , "900" 
@@ -23,6 +25,7 @@ export default function RootLayout({
         <NextAuthProvider>
           <Header />
           {children}
+          <Footer />
         </NextAuthProvider>
         </body>
     </html>
