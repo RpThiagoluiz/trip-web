f1bda12c785217ff8afcdd6093b3a2776cfbd0c2
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-07-08 11:10:09 -0300

    fix: optional props in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,25 @@
-import { ComponentPropsWithoutRef } from "react";
-import { twMerge } from "tailwind-merge";
+import { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  variant: "primary" | "outlined";
+interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
+  variant?: 'primary' | 'outlined';
 }
 
 const variantClasses = {
-  primary: "bg-primary text-white hover:bg-primaryDarker",
-  outlined: "bg-transparent border-2 border-primary text-primary",
+  primary: 'bg-primary text-white hover:bg-primaryDarker',
+  outlined: 'bg-transparent border-2 border-primary text-primary',
 };
 
-export default function Button({ className, variant = "primary", ...rest }: ButtonProps) {
-  
-  const _className = twMerge(variantClasses[variant], "appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all", className);
+export default function Button({
+  className,
+  variant = 'primary',
+  ...rest
+}: ButtonProps) {
+  const _className = twMerge(
+    variantClasses[variant],
+    'appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all',
+    className
+  );
 
   return (
     <button className={_className} {...rest}>
